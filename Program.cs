dc432340690c357d39df7b6a1e8c1f34e2884f24
Harsh <harsh.21483@gmail.com>
2021-06-20 08:43:03 -0700

    [Harsh]Add:- New class Regex validate is added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace REGEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User registration Problem statement");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string name = Console.ReadLine();
+            RegexValidation regex = new RegexValidation();
+            bool validName = regex.ValidateFirstName();
+            Console.WriteLine(validName);
+        }
+    }
+}
---
+++ RegexValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace REGEX
+{
+    public class RegexValidation
+    {
+        public static string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{6,}$";
+        
+
+        public bool ValidateFirstName()
+        {
+            return Regex.IsMatch("Harshsinha", NAME_REGEX);
+        }
+        
+    }
+}
